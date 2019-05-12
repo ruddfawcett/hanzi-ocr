510c0efde49ece2b6106981c1766352c5a8c9eb4
Rudd Fawcett <rudd.fawcett@gmail.com>
2019-05-12 18:41:19 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,14 +1,2 @@
-
-When you are working with Optical character recognition(OCR) or any data or 
-object recognition problem, the first thing to do is preprocessing. Here preprocessing means
-to extract the location where our information is located. After extracting the location,
-any machine algorithm will be performed on that image. 
-
-The problem arises when you have to detect objects which are located in any tables/boxes or
-in row-column format. If the image is like this then you have to detect boxes and extract them one by one. 
-Now it should be done accurately for all images.
-
-This algorithm helps to detect every boxes accurately and save it in a "Cropped" folder.The code is shown in box_detection.py 
-and the test image is "41.jpg".
-
-you can see the medium blog for this code: https://medium.com/@kananvyas/a-box-detection-algorithm-for-any-image-containing-boxes-756c15d7ed26
+# hanzi-ocr
+Detect and compare handwritten Chinese characters using Google Cloud Vision OCR.
--- character_box_detection.py
+++ character_box_detection.py
@@ -1,5 +1,24 @@
+'''
+Taken almost without modification from Kanan Vyas's tutorial on
+box detection with OpenCV. https://github.com/KananVyas/BoxDetection
+'''
+
 import cv2
 import numpy as np
+
+import io
+import os
+
+# export GOOGLE_APPLICATION_CREDENTIALS=hanzi-ocr-6187fe679c36.json
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud import translate
+from google.cloud.vision import types
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+translate_client = translate.Client()
 
 def sort_contours(cnts, method="left-to-right"):
     # initialize the reverse flag and sort index
@@ -24,18 +43,60 @@ def sort_contours(cnts, method="left-to-right"):
     # return the list of sorted contours and bounding boxes
     return (cnts, boundingBoxes)
 
-def box_extraction(img_for_box_extraction_path, cropped_dir_path):
+def detect_text(path):
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+        print(path)
 
+    image = vision.types.Image(content=content)
+
+    response = client.document_text_detection(
+        image=image,
+        image_context={"language_hints": ["zh"]})
+
+    annotations = response.text_annotations
+
+    if len(annotations) > 0:
+        text = annotations[0].description
+    else:
+        text = ''
+    print('Extracted text {} from image ({} chars).'.format(text, len(text)))
+
+    detect_language_response = translate_client.detect_language(text)
+    src_lang = detect_language_response['language']
+    print('Detected language {} for text {}.'.format(src_lang, text))
+
+    # # if len(annotations) > 0:
+    # #     text = annotations[0].description
+    # # else:
+    # #     text = ''
+    # text = ''
+    # for t in annotations:
+    #     text = t.description
+    # print('Extracted text {} from image ({} chars).'.format(text, len(text)))
+
+    # texts = response.text_annotations
+    # string = ''
+    #
+    # if (len(texts) > 0):
+    #     string =  texts[0].description
+    #
+    # for text in texts:
+    #     string+=' ' + text.description
+
+    # return '$$ ' + string + ' $$'
+
+def box_extraction(img_for_box_extraction_path, cropped_dir_path):
     img = cv2.imread(img_for_box_extraction_path, 0)  # Read the image
     (thresh, img_bin) = cv2.threshold(img, 128, 255,
                                       cv2.THRESH_BINARY | cv2.THRESH_OTSU)  # Thresholding the image
     img_bin = 255-img_bin  # Invert the image
 
-    cv2.imwrite("Image_bin.jpg",img_bin)
-   
+    # cv2.imwrite("./test/image_bin.jpg",img_bin)
+
     # Defining a kernel length
     kernel_length = np.array(img).shape[1]//40
-     
+
     # A verticle kernel of (1 X kernel_length), which will detect all the verticle lines from the image.
     verticle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length))
     # A horizontal kernel of (kernel_length X 1), which will help to detect all the horizontal line from the image.
@@ -46,12 +107,12 @@ def box_extraction(img_for_box_extraction_path, cropped_dir_path):
     # Morphological operation to detect verticle lines from an image
     img_temp1 = cv2.erode(img_bin, verticle_kernel, iterations=3)
     verticle_lines_img = cv2.dilate(img_temp1, verticle_kernel, iterations=3)
-    cv2.imwrite("verticle_lines.jpg",verticle_lines_img)
+    # cv2.imwrite("./test/verticle_lines.jpg",verticle_lines_img)
 
     # Morphological operation to detect horizontal lines from an image
     img_temp2 = cv2.erode(img_bin, hori_kernel, iterations=3)
     horizontal_lines_img = cv2.dilate(img_temp2, hori_kernel, iterations=3)
-    cv2.imwrite("horizontal_lines.jpg",horizontal_lines_img)
+    # cv2.imwrite("./test/horizontal_lines.jpg",horizontal_lines_img)
 
     # Weighting parameters, this will decide the quantity of an image to be added to make a new image.
     alpha = 0.5
@@ -63,28 +124,34 @@ def box_extraction(img_for_box_extraction_path, cropped_dir_path):
 
     # For Debugging
     # Enable this line to see verticle and horizontal lines in the image which is used to find boxes
-    cv2.imwrite("img_final_bin.jpg",img_final_bin)
+    # cv2.imwrite("./test/img_final_bin.jpg", img_final_bin)
     # Find contours for image, which will detect all the boxes
-    im2, contours, hierarchy = cv2.findContours(
-        img_final_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # Sort all the contours by top to bottom.
+    contours, hierarchy = cv2.findContours(img_final_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Sort all the contours by left to right.
+    (contours, boundingBoxes) = sort_contours(contours, method="left-to-right")
+        # Sort all the contours by top to bottom.
     (contours, boundingBoxes) = sort_contours(contours, method="top-to-bottom")
 
     idx = 0
+    # Need these threshold to get just each individual character square.
+    # Have not yet tested with just 口, only images with kou radical
+    square_threshold = 2.0
+
     for c in contours:
         # Returns the location and width,height for every contour
         x, y, w, h = cv2.boundingRect(c)
-
-        # If the box height is greater then 20, widht is >80, then only save it as a box in "cropped/" folder.
-        if (w > 80 and h > 20) and w > 3*h:
+        # If the box is essentially a square, then only save it as a box in "characters/" folder.
+        if (abs(w - h) < square_threshold):
             idx += 1
             new_img = img[y:y+h, x:x+w]
-            cv2.imwrite(cropped_dir_path+str(idx) + '.png', new_img)
+            cv2.imwrite(cropped_dir_path + str(idx) + '.png', new_img)
+            text = detect_text(cropped_dir_path + str(idx) + '.png')
+            print(text)
 
     # For Debugging
     # Enable this line to see all contours.
     # cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
-    # cv2.imwrite("./Temp/img_contour.jpg", img)
+    # cv2.imwrite("./temp/img_contour.jpg", img)
 
-
-box_extraction("41.jpg", "./Cropped/")
+box_extraction("en_test-multi-blank.png", "./characters/")
